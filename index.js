f46cf88dd70da4fff723d2db1b279c356be55bae
GabbyPly <gabby@ply.io>
2022-06-01 09:00:12 +0300

    acgions/github in modules + index.js and action.yml include code from example in github

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
+// code ex from github-hello-world-js-action
 const core = require('@actions/core');
+const github = require('@actions/github');
 
-const myInput2 = core.getInput('changed_files', { required: false });
-console.log('myInput2', myInput2);
-const myInput3 = core.getInput('changed_files_two', { required: true });
-console.log('myInput3', myInput3);
-// const myInput3 = core.getInput('INPUT_FAM_NAME', { required: true }); // does not work
-
-// TODO strong chance that we will require a multiLineInput
-// const myMultilineInput = core.getMultilineInput('multilineInputName', {
-//     required: true,
-// });
-
-// These will be passed in the above step
-const calendly = '/Users/gabrielmillatiner/dev/ply-app-integrations/calendly';
-const bitly = '/Users/gabrielmillatiner/dev/ply-app-integrations/bitly';
-const mindee = '/Users/gabrielmillatiner/dev/ply-app-integrations/mindee';
-
-const array = [calendly, bitly, mindee];
-
-// should return the command itself already, something such as:
-// npm run release-app-staging -w bitly -w one-drive
-
-core.setOutput('outputKey', array.join(' '));
-// core.setOutput('release_cmd', array.join(' '));
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    core.setOutput('time', time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
